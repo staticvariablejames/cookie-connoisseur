d1d21b6e596099f18106f04b605c0384fe662292
Static Variable James <staticvariablejames@gmail.com>
2021-03-14 22:04:49 -0500

    openCookieClickerPage: Use cache/ pages.

--- src/cookie-clicker-page.ts
+++ src/cookie-clicker-page.ts
@@ -1,7 +1,37 @@
 import { Browser } from 'playwright';
+import { existsSync } from 'fs';
+import { urlSet } from './url-list.js';
 
+/* Uses the given browser to navigate to https://orteil.dashnet.org/cookieclicker/index.html
+ * in a new page.
+ * Routes that query orteil.dashnet.org will be redirected to use the local cache instead.
+ */
 export async function openCookieClickerPage(browser: Browser) {
     let page = await browser.newPage();
-    await page.goto('https://orteil.dashnet.org/cookieclicker/');
+
+    await page.route('https://orteil.dashnet.org/**/*', route => {
+        let path = route.request().url();
+        path = path.replace('https://orteil.dashnet.org/', '');
+        /* For some reason, some URLs have a version attached at the end
+         * (like 'main.js?v=2.089' instead of just 'main.js'),
+         * even though accessing 'https://orteil.dashnet.org/cookieclicker/main.js'
+         * yields the same page as 'main.js?v=2.089' and 'main.js?v=2.058'.
+         * So we just strip that part out of the equation.
+         */
+        path = path.replace(/\?v=.*/, '').replace(/\?r=.*/, '');
+        console.log(`Request for ${route.request().url()} (path is ${path})...`);
+
+        if(urlSet.has(path.replace('cookieclicker/',''))) {
+            if(existsSync('cache/' + path)) {
+                route.fulfill({path: 'cache/' + path});
+            } else {
+                console.log(`File ${path} not cached`);
+            }
+        } else {
+            console.log(`Unknown file ${path}, won't use cache...`);
+        }
+    });
+
+    await page.goto('https://orteil.dashnet.org/cookieclicker/index.html');
     return page;
 }
--- src/url-list.ts
+++ src/url-list.ts
@@ -1,3 +1,5 @@
 export let urls = [
     {url: 'index.html'}
 ];
+
+export const urlSet = new Set( urls.map( v => v.url ) );
