d5ada653fcbd486dcb2ac21389cd699a6e39d3fe
Static Variable James <staticvariablejames@gmail.com>
2021-03-27 16:22:26 -0500

    Replace Firefox with Chromium for downloading files
    For some reason, even when headless,
    Firefox still emits sounds when navigating to the sound files.
    This patch replaces it with Chromium, which is actually silent.
    
    Chromium also seems to continue navigation
    before all asynchronous event listeners finish running,
    so some extra care had to be done to avoid "dangling references".

--- src/fetch-cookie-clicker-files.ts
+++ src/fetch-cookie-clicker-files.ts
@@ -1,26 +1,43 @@
 import { promises as fsPromises } from 'fs';
 import { dirname } from 'path';
 import { localPathOfURL } from './cookie-clicker-cache.js';
-import { firefox } from 'playwright';
+import { chromium, Page, Response } from 'playwright';
 import { cacheURLs } from './url-list.js';
 
-setTimeout(async () => {
-    let browser = await firefox.launch();
-    let page = await browser.newPage();
-    let path: string;
-    let currentURL: string;
-    page.on('response', async response => {
-        console.log('Request for ' + response.url());
-        if(Math.floor(response.status()/100) == 2 && response.url() == currentURL) { // Success
-            console.log('Writing ' + path);
-            await fsPromises.writeFile(path, await response.body());
-        }
-    })
-    for(let url in cacheURLs) {
-        currentURL = url;
-        path = localPathOfURL(url);
-        await fsPromises.mkdir(dirname(path), {recursive:true});
+async function fetchUrl(page: Page, url: string) {
+    if(url.endsWith('/favicon.ico')) {
+        /* Playwright does not emit events if the URL ends in '/favicon.ico'.
+         * I'm not sure why;
+         * Googling suggests it is because browsers use a favicon cache.
+         * TODO: implement alternative favicon downloader.
+         */
+        return;
+    }
+
+    console.log('Downloading ' + url);
+    let currentURL = url;
+    let path = localPathOfURL(url);
+    await fsPromises.mkdir(dirname(path), {recursive:true});
+
+    await new Promise(async resolve => {
+        let handler = async (response: Response) => {
+            if(response.ok() && response.url() == currentURL) { // Success
+                await fsPromises.writeFile(path, await response.body());
+                await page.removeListener('response', handler);
+                resolve(true);
+            }
+        };
+        await page.on('response', handler);
         await page.goto(url);
+    });
+};
+
+setTimeout(async () => {
+    let browser = await chromium.launch();
+    let page = await browser.newPage();
+
+    for(let url in cacheURLs) {
+        await fetchUrl(page, url);
     }
     await page.close();
     await browser.close();
