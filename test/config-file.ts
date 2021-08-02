f936a342a67331a726892a39ef9cd2a59393738b
Static Variable James <staticvariablejames@gmail.com>
2021-08-01 20:34:12 -0500

    Test that customURLs routes can still be overriden manually.

--- test/config-file.ts
+++ test/config-file.ts
@@ -1,5 +1,6 @@
 /* Simple test for the configuration file.
  */
+/// <reference path="../src/browser-utilities.d.ts" />
 
 import { CCPageOptions, openCookieClickerPage } from '../lib/cookie-clicker-page.js';
 import { parseConfigFile } from '../lib/parse-config.js';
@@ -21,8 +22,23 @@ test('Configuration file affects the page', async () => {
     let page = await newPage();
     await page.evaluate(() => Game.LoadMod("https://example.com/test.js"));
     await page.waitForFunction(() => 'testWorks' in window.CConnoisseur);
-    let test = await page.evaluate(() => (window.CConnoisseur as any).testWorks);
-    expect(test).toBe(true);
+    expect(await page.evaluate(() => (window.CConnoisseur as any).testWorks)).toBe(true);
+    await page.close();
+});
+
+test('Configuration file reroutes can be overriden', async () => {
+    let page = await newPage();
+    await page.route("https://example.com/test.js", route => {
+        route.fulfill({
+            contentType: 'application/javascript',
+            body: "window.CConnoisseur.overridingWorks = true;"
+        });
+    });
+
+    await page.evaluate(() => Game.LoadMod("https://example.com/test.js"));
+    await page.waitForFunction(() => 'overridingWorks' in window.CConnoisseur);
+    expect(await page.evaluate(() => (window.CConnoisseur as any).overridingWorks)).toBe(true);
+    expect(await page.evaluate(() => 'testWorks' in window.CConnoisseur)).toBe(false);
     await page.close();
 });
 
