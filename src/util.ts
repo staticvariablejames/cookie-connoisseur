55254a249755a274697615c1e407aaa0eef03fb2
Static Variable James <staticvariablejames@gmail.com>
2021-08-23 09:45:58 -0500

    util.ts: Add pseudoObjectAssign.

--- src/util.ts
+++ src/util.ts
@@ -10,3 +10,62 @@ export function invertMap( map: string[] ) : { [key: string] : number } {
     }
     return inverseMap;
 }
+
+export type ErrorHandler = (msg: string) => void;
+
+/* Simple implementation of an ErrorHandler,
+ * which simply throws an error with the given message.
+ */
+export const throwOnError: ErrorHandler = (msg: string) => {
+    throw new Error(msg);
+}
+
+/* This is essentially a strict Object.assign.
+ * Every attribute of the source is assigned to the corresponding attribute of the target,
+ * but the attribute must exist in the target,
+ * the types must match,
+ * and the attribute must be a string, number or boolean
+ * (so functions, arrays and objects are ignored).
+ *
+ * The subobjectName attribute is used to generate more helpful error messages.
+ * By default, the function complains that e.g. "source.amount is not a number".
+ * Setting subobjectName to '.wrinklers' changes it to "source.wrinklers.amount is not a number".
+ */
+export function pseudoObjectAssign<T>(
+    target: T,
+    source: any,
+    onError: ErrorHandler,
+    subobjectName: string = ''): T
+{
+    // TODO: improve typings
+    let _target = target as any;
+    if(typeof _target != 'object' || _target == null) {
+        onError('target is not a non-null object');
+        return _target;
+    }
+    if(typeof source != 'object' || source == null) {
+        onError('source is not a non-null object');
+        return _target;
+    }
+
+    // Copy valid keys
+    const validTypes = new Set(['number', 'boolean', 'string']);
+    for(let key of Object.keys(_target)) {
+        if(!(key in source)) continue;
+        if(!validTypes.has(typeof _target[key])) continue;
+        if(typeof _target[key] == typeof source[key]) {
+            _target[key] = source[key];
+        } else {
+            onError(`source${subobjectName}.${key} is not a ${typeof _target[key]}`);
+        }
+    }
+
+    // Complain about keys which don't exist on the target
+    for(let key of Object.keys(source)) {
+        if(!(key in _target)) {
+            onError(`target${subobjectName}.${key} does not exist (typo?)`);
+        }
+    }
+
+    return _target;
+}
--- test/util.test.ts
+++ test/util.test.ts
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test';
-import { invertMap } from '../src/util';
+import { invertMap, pseudoObjectAssign, throwOnError } from '../src/util';
 
 test('invertMap inverts maps', () => {
     let map = ['zero', 'one', 'two'];
@@ -8,5 +8,101 @@ test('invertMap inverts maps', () => {
         'zero': 0,
         'one': 1,
         'two': 2,
+    });
+});
+
+test.describe('pseudoObjectAssign', () => {
+    // Simple mocking
+    function makeOnErrorMock() {
+        let obj = {
+            msg: '',
+            onError: (msg: string) => {
+                obj.msg = msg;
+            },
+        };
+        return obj;
+    }
+
+    test('works for full copy', () => {
+        let mock = makeOnErrorMock();
+        let target = {a: 6, b: 'six', c: false};
+        let source = {a: 5, b: 'five', c: true};
+        let out = pseudoObjectAssign(target, source, mock.onError);
+        expect(out).toBe(target);
+        expect(out).toEqual(source);
+        expect(mock.msg).toEqual('');
+    });
+
+    test('works for partial copy', () => {
+        let mock = makeOnErrorMock();
+        let target = {a: 6, b: 'six', c: false};
+        let out = pseudoObjectAssign(target, {a: 7}, mock.onError);
+        expect(out).toEqual({a: 7, b: 'six', c: false});
+        expect(mock.msg).toEqual('');
+
+        out = pseudoObjectAssign(target, {b: 'seven', c: true}, mock.onError);
+        expect(out).toEqual({a: 7, b: 'seven', c: true});
+        expect(mock.msg).toEqual('');
+    });
+
+    test('complains about mismatched types', () => {
+        let mock = makeOnErrorMock();
+        let out = pseudoObjectAssign({a: 5}, {a: true}, mock.onError);
+        expect(mock.msg).toContain('source.a is not a number');
+        expect(out).toEqual({a: 5});
+    });
+
+    test('complains about extraneous keys', () => {
+        let mock = makeOnErrorMock();
+        let out = pseudoObjectAssign({a: 5}, {a: 3, b: true}, mock.onError);
+        expect(mock.msg).toContain('target.b does not exist');
+        expect(out).toEqual({a: 3}); // Complains but do its job
+    });
+
+    test('complains if arguments are not objects', () => {
+        let mock = makeOnErrorMock();
+        pseudoObjectAssign('a', {}, mock.onError);
+        expect(mock.msg).toContain('target is not a non-null object');
+
+        mock = makeOnErrorMock();
+        pseudoObjectAssign({}, 'a', mock.onError);
+        expect(mock.msg).toContain('source is not a non-null object');
+    });
+
+    test('leaves alone arrays, functions, and objects', () => {
+        let mock = makeOnErrorMock();
+        let originalTarget = {
+            a: [1],
+            b: (x: number) => 2 * x,
+            c: {d: 5},
+        };
+        let target = {...originalTarget};
+        let source = {
+            a: [2],
+            b: (x: number) => 3 * x,
+            c: {d: 6},
+        };
+        let obj = pseudoObjectAssign(target, source, mock.onError);
+        expect(obj).toEqual(originalTarget);
+        expect(mock.msg).toBe('');
+    });
+
+    test('complains multiple times if needed', () => {
+        let messages: string[] = [];
+        let onError = (msg: string) => {messages.push(msg)};
+        pseudoObjectAssign({a: true}, {a: 'true', b: false}, onError);
+        expect(messages.length).toBe(2);
+        expect(messages[0]).toContain('source.a is not a boolean');
+        expect(messages[1]).toContain('target.b does not exist');
+    });
+
+    test('modifies its complaints according to subobjectName', () => {
+        let mock = makeOnErrorMock();
+        pseudoObjectAssign({}, {a: 1}, mock.onError, '.wrinklers');
+        expect(mock.msg).toContain('target.wrinklers.a does not exist');
+
+        mock = makeOnErrorMock();
+        pseudoObjectAssign({a: 1}, {a: true}, mock.onError, '["Wizard towers"]');
+        expect(mock.msg).toContain('source["Wizard towers"].a is not a number');
     });
 });
